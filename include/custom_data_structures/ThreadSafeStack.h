54528445444470e47d083986ef440f220ec629af
mateusz <mateusz0990@gmail.com>
2025-08-18 21:17:31 +0200

    Add Initial ThreadSafeStack implementation

---
+++ include/custom_data_structures/ThreadSafeStack.h
@@ -0,0 +1,54 @@
+#include <mutex>
+
+template<typename T> class ThreadSafeStack {
+private:
+    std::vector<T> stack;
+    mutable std::mutex mutex;
+    bool isEmptyFlag = true;
+
+public:
+    ThreadSafeStack() = default;
+
+    void push(const T& element) {
+        mutex.lock();
+        stack.push_back(element);
+        isEmptyFlag = false;
+        mutex.unlock();
+    }
+
+    T pop() {
+        if(isEmptyFlag) {
+            throw std::runtime_error("Stack is empty");
+        }
+        mutex.lock();
+        T element = stack.back();
+        stack.pop_back();
+        isEmptyFlag = stack.empty();
+        mutex.unlock();
+        return element;
+    }
+
+    T& top() {
+        if(isEmptyFlag) {
+            throw std::runtime_error("Stack is empty");
+        }
+        mutex.lock();
+        T& element = stack.back();
+        mutex.unlock();
+        return element;
+    }
+
+    bool isEmpty() const {
+        mutex.lock();
+        bool empty = isEmptyFlag;
+        mutex.unlock();
+        return empty;
+    }
+
+    int size() const {
+        mutex.lock();
+        int size = stack.size();
+        mutex.unlock();
+        return size;
+    }
+};
