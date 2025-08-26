f7c197ad3bbb31ce45cd96a6112c2605a029090f
mateusz <mateusz0990@gmail.com>
2025-08-26 22:44:25 +0200

    Add DIjkstra Algorithm

---
+++ include/algorithms/Dijkstra.h
@@ -0,0 +1,64 @@
+#pragma once
+#include "Algorithm.h"
+#include "utils/Graph.h"
+#include <vector>
+#include <algorithm>
+#include <limits>
+
+using namespace std;
+
+class Dijkstra : public Algorithm {
+private:
+    vector<int> distances;
+    vector<int> shortestPath;
+    vector<int> shortestPathResult;
+    Graph& graph;
+
+    void dijkstraStlSearch(int start, int end);
+    void dijkstraCustomSearch(int start, int end);
+
+    template<typename PriorityQueueType>
+    void dijkstraSearch(int start, int end, PriorityQueueType& pq) {
+        distances.assign(graph.getNumVertices(), numeric_limits<int>::max());
+        shortestPath.assign(graph.getNumVertices(), -1);
+        distances[start] = 0;
+        pq.push({0, start});
+
+        const vector<vector<int>>& adjacencyMatrix = graph.getAdjacencyMatrix();
+
+        while (!pq.empty()) {
+            auto [dist, current] = pq.top();
+            pq.pop();
+
+            if (current == end) {
+                reconstructPath(start, end);
+                return;
+            }
+
+            for (int neighbor = 0; neighbor < adjacencyMatrix.size(); ++neighbor) {
+                if (adjacencyMatrix[current][neighbor] == 0) continue;
+                int newDist = distances[current] + graph.getEdge(current, neighbor);
+                if (newDist < distances[neighbor]) {
+                    distances[neighbor] = newDist;
+                    shortestPath[neighbor] = current;
+                    pq.push({newDist, neighbor});
+                }
+            }
+        }
+    }
+
+    void reconstructPath(int start, int end) {
+        shortestPathResult.clear();
+        for (int v = end; v != -1; v = shortestPath[v]) {
+            shortestPathResult.push_back(v);
+        }
+        reverse(shortestPathResult.begin(), shortestPathResult.end());
+    }
+
+public:
+    Dijkstra(Graph& g);
+    ~Dijkstra();
+
+    void run(int start, int end, bool useCustomDataStructure);
+    const vector<int>& getShortestPath() const;
+};
---
+++ src/algorithms/Dijkstra.cpp
@@ -0,0 +1,41 @@
+#include "algorithms/Dijkstra.h"
+#include <queue>
+#include "custom_data_structures/ThreadSafePriorityQueue.h"
+#include <iostream>
+
+using namespace std;
+
+Dijkstra::Dijkstra(Graph& g) : graph(g) {}
+
+Dijkstra::~Dijkstra() {}
+
+void Dijkstra::run(int start, int end, bool useCustomDataStructure) {
+    if (useCustomDataStructure) {
+        Dijkstra::dijkstraCustomSearch(start, end);
+    } else {
+        Dijkstra::dijkstraStlSearch(start, end);
+    }
+
+    for (size_t i = 0; i < graph.getNumVertices(); i++)
+    {
+        cout << distances[i] << " ";
+    }
+    cout << endl;
+    
+
+    for (int i = 0; i < shortestPathResult.size(); i++)
+    {
+        cout << shortestPathResult[i] << " ";
+    }
+    cout << endl;
+}
+
+void Dijkstra::dijkstraStlSearch(int start, int end) {
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    dijkstraSearch(start, end, pq);
+}
+
+void Dijkstra::dijkstraCustomSearch(int start, int end) {
+    ThreadSafePriorityQueue<pair<int, int>, greater<pair<int, int>>> pq;
+    dijkstraSearch(start, end, pq);
+}
